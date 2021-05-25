5033be64a7a44172018ab517dd209c4957477842
Kacper Herchel <kacperski1@protonmail.com>
2021-05-25 20:10:43 +0200

    More debug data

--- src/api/avatars.ts
+++ src/api/avatars.ts
@@ -7,15 +7,21 @@ interface AvatarLoader {
 export const avatars: { [key: string]: AvatarLoader } = {};
 
 export const loadAvatarURL = (steamid: string) => {
+    console.log('Loading avatar for:', steamid)
     if(!steamid) return;
-    if(avatars[steamid]) return avatars[steamid].url;
+    if(avatars[steamid]) {
+        console.log('Avatar already loaded:', avatars[steamid])
+        return avatars[steamid].url;
+    }
     avatars[steamid] = {
         url: '',
         loader: new Promise((resolve) => {
             api.players.getAvatarURLs(steamid).then(result => {
+                console.log('Avatar API result:', result)
                 avatars[steamid].url = result.custom || result.steam;
                 resolve(result.custom || result.custom);
-            }).catch(() => {
+            }).catch(e => {
+                console.log('Avatar API fail! Error:', e)
                 delete avatars[steamid];
                 resolve('');
             });
