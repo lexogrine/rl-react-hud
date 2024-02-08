749c6233e9a854e571352829cfab68dbe8d0b371
Hubert Walczak <hubertwalczak8@gmail.com>
2024-02-08 10:13:29 +0100

    update deps

--- src/lhm-rl-module/RL.ts
+++ src/lhm-rl-module/RL.ts
@@ -1,4 +1,3 @@
-import fs from "fs";
 import {
   Game,
   RawGame,
@@ -200,6 +199,7 @@ export default class RL {
       new Date().getTime() - this.debugLastDumpTime.getTime() >=
         this.debugDumpFrequencySeconds * 1000
     ) {
+      /*
       fs.appendFile(
         "events.lhmd",
         this.events
@@ -214,7 +214,7 @@ export default class RL {
             this.debugLastDumpIndex = this.events.length - 1;
           }
         }
-      );
+      );*/
     }
   };
 
